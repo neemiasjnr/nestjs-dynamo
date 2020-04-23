6cc011a31703884c5c4adde6bf511c5a9c937eda
Neemias Junior <neemias.junior@gmail.com>
2020-04-23 16:29:05 +0200

    Initial commit

--- src/dynamo.service.ts
+++ src/dynamo.service.ts
@@ -2,12 +2,12 @@ import { Inject, Injectable } from '@nestjs/common';
 import DynamoDB = require('aws-sdk/clients/dynamodb');
 
 import { DataMapper } from '@aws/dynamodb-data-mapper';
-import { DatabaseModuleOptions, IDatabaseService } from './dynamo.interface';
+import { DynamoOptions, IDynamoService } from './dynamo.interface';
 import { DYNAMO_OPTIONS } from './dynamo';
 
 @Injectable()
-export class DynamoService implements IDatabaseService {
-  constructor(@Inject(DYNAMO_OPTIONS) private options: DatabaseModuleOptions) {}
+export class DynamoService implements IDynamoService {
+  constructor(@Inject(DYNAMO_OPTIONS) private options: DynamoOptions) {}
 
   async connect(): Promise<DataMapper> {
     const client = new DynamoDB(this.options);
